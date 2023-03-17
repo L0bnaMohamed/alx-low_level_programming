bd034d6174a6d23500a4a937cc66b14004f68b6a
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-17 12:03:10 -0700

    new printf commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description:Print "Programming is like building a multilingual puzzle"
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
