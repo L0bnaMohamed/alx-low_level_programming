f8bca33569ccaf43d0c1b28996ba74a361e505d7
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-21 16:39:48 -0700

    absulute task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs(int)- is a functin to print absulute
+ * @i: is an integer
+ * Return: Always (success)
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+
+}
