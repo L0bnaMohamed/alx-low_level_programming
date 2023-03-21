7b2b22e46e63c280eaa4c7e4d3862e02905c998e
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-21 07:43:16 -0700

    new task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	int putch[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int size = sizeof(putch) / sizeof(int);
+	int putch[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(putch[i]);
 	}
