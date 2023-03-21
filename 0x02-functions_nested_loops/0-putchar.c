0e463858934e11c9f806a44df441a3b1e81da6a4
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-21 05:48:49 -0700

    new task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descrioptin: program that prints _putchar
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int putch[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	for (int i = 0; i < 8; i++)
+	{
+	putchar(putch[i]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file for the directory #0x02-functions_nested_loop
