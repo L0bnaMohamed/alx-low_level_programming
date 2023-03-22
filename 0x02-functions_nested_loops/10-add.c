3a075adcd8afc711894e1eb76a368ad0b6db6588
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-21 17:30:59 -0700

    add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add(int, int)- function that adds two integers
+ * and returns the result.
+ * @i: is an integer
+ * @j: is an integer
+ * Return: (success)
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
