7db5f0e733bc30a8d4e5c153060fbcfa32303f60
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-23 18:26:11 -0700

    print 1 if the charcter is capital

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper(int c) -function
+ * @c: is an integer
+ * Return: return
+ */
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
