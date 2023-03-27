cbc3a3c58c9bd0c867d0d6963d554a1dcd115f24
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-27 15:20:38 -0700

    puts2 task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2(char *str)- is the function
+ * @str: is a pointer
+ */
+void puts2(char *str)
+{
+	int i = 0, len, k;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i - 1;
+	for (k = 0; k <= len ; k += 2)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
