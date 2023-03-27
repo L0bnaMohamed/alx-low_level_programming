b7197ee2773551d09d4c804917a0b97864cfcee4
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-27 13:27:08 -0700

    print rev task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev(char *s)- function
+ *
+ * @s: is a pointer
+ *
+ * Return: return
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 64; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
