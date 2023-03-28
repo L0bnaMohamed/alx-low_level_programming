81b5a7d59c1dc69d68f7fb9d9c082e4389772116
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-03-28 08:53:48 -0700

    puts half task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half(char *str)-is the ufnction
+ * @str: is a pointer
+ */
+void puts_half(char *str)
+{
+	int i = 0, len, k, n;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i - 1;
+	n = (len / 2);
+	for (k = n + 1; k <= len ; k ++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
+}
