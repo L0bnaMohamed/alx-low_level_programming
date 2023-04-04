3fddd944ff8686d2d4e4b488815b9a1d8b744834
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-03 18:00:33 -0700

    fill the memory with the constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset(char *s, char b, unsigned int n)-function that fill
+ * the memory with a constant byte
+ * @s: is pointer
+ * @b: is a character
+ * @n: is an integer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int number;
+
+	for (number = 0; number < n; number++)
+	{
+		s[number] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
