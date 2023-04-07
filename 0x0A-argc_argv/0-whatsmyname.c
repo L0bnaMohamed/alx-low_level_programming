2daff77f5a3a826d9a2d46e40e04bc2a820720fa
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-07 06:51:01 -0700

    new directory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is the function
+ *
+ * @argc: is the string size
+ *
+ * @argv: is a string
+ *  Return: return functin
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (1);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Readme file of the 0x0A-argc_argv
