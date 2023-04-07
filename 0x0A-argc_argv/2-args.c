670647f98465d068e23f3809653112f754966456
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-07 07:42:20 -0700

    prints all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - is the function
+ *
+ * @argc: is the string size
+ *
+ * @argv: is a string
+ *  Return: return functin
+ */
+int main(int argc, char *argv[])
+{
+	int number_of_argument;
+
+	for ( number_of_argument = 0; number_of_argument < argc; number_of_argument++)
+	{
+		printf("%s\n", argv[number_of_argument]);
+	}
+	return (0);
+}
