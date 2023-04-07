a6e3c295e8e790d3a36c069155e06679b5002120
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-07 07:30:22 -0700

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,14 @@
 
 /**
  * main - is the function
- * __attribute__((unused))-function used to ignore
- * usage of the variable
  * @argc: is the string size
- *
  * @argv: is a string
  *  Return: return 0 success
  */
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
 	return (0);
 }
