8c389a8b29b547b9930a357ce74767fa30ea6014
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-10 18:40:57 -0700

    the main function

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - is a function
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker
+\"\n\t- Kevin Mitnick");
+	return (0);
+}
