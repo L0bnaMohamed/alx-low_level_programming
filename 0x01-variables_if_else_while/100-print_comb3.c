67747f434b00a2ac28b226fa11055f7a296ea265
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-21 06:11:48 -0700

    task advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main- is the main function
+ * Return: is the return
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == 8 && j == 9)
+			{
+				printf("%d%d\n", i, j);
+				break;
+			}
+			else if (i == 0 && j == 0)
+			{
+				printf("%d%d", i, j);
+				putchar(',');
+				putchar(' ');
+			}
+			else if (i == 0 && j > 0 && j < 9)
+			{
+				printf("%d%d", i, j);
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				printf("%d%d", i, j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+		return (0);
+}
