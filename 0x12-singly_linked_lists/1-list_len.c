88c82e68ecdd1dceab717acba3dd953c51961516
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-27 12:17:04 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Function to prints all the elements
+ * of a list_t list.
+ * @h: Pointer to the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
