2935182e7dbc626154aad6523542a59854249f50
L0bnaMohamed <lobnamohamed585@gmail.com>
2023-04-29 07:14:20 -0700

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,64 +1,57 @@
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
+#define MAX_WORDS 100
 
+/**
+ * strtow- is the function
+ * @str: is a char
+ * Return : return
+ */
 char **strtow(char *str)
 {
-	int  word_index = 0, i, j, word_len, num_words = 1;
-	char *word_start = str;
-	char **words = malloc((num_words + 1) * sizeof(char *));
+	char **words = malloc((MAX_WORDS + 1) * sizeof(char *)), *end;
+	int i = 0, j, len;
 
-	if (str == NULL || strcmp(str, "") == 0)
+	if (str == NULL || *str == '\0')
 	{
 		return (NULL);
 	}
-       for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-		{
-			num_words++;
-		}
-	}
-    
 	if (words == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str != '\0' && i < MAX_WORDS)
 	{
-		if (str[i] == ' ')
+		while (*str == ' ')
 		{
-			word_len = &str[i] - word_start;
-			words[word_index] = malloc((word_len + 1) * sizeof(char));
-			if (words[word_index] == NULL)
-			{
-				for (j = 0; j < word_index; j++)
-				{
-				free(words[j]);
-				}
-				free(words);
-				return (NULL);
-			}
-			strncpy(words[word_index], word_start, word_len);
-			words[word_index][word_len] = '\0';
-			word_index++;
-			word_start = &str[i + 1];
+			str++;
 		}
-	}
-
-	word_len = &str[strlen(str)] - word_start;
-	words[word_index] = malloc((word_len + 1) * sizeof(char));
-	if (words[word_index] == NULL) {
-	for (j = 0; j <= word_index; j++)
+		if (*str == '\0')
+		{
+			break;
+		}
+		end = str;
+		while (*end != ' ' && *end != '\0')
+		{
+			end++;
+		}
+	len = end - str;
+	words[i] = malloc((len + 1) * sizeof(char));
+	if (words[i] == NULL)
+	{
+	for (j = i - 1; j >= 0; j--)
 	{
 		free(words[j]);
 	}
 	free(words);
 	return (NULL);
 	}
-	strncpy(words[word_index], word_start, word_len);
-	words[word_index][word_len] = '\0';
-
-	words[num_words] = NULL;
-
+	strncpy(words[i], str, len);
+	words[i][len] = '\0';
+	i++;
+	str = end;
+	}
+	words[i] = NULL;
 	return (words);
 }
